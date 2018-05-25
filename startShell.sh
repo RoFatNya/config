2adf847b91178a31201d3c181df1a7b20f33a65d
RoFatNya <jiaye1@qq.com>
2018-05-25 14:26:30 +0800

    Update startShell.sh

--- startShell.sh
+++ startShell.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-
+#
 #download
-
+#
 #haproxy
 wget https://raw.githubusercontent.com/RoFatNya/haproxy/master/haproxy-1.8.5.tar.gz
 tar -zxvf haproxy-1.8.5.tar.gz
@@ -12,44 +12,44 @@ cp haproxy /usr/sbin/haproxy
 mkdir /etc/haproxy
 wget /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/RoFatNya/config/master/haproxy/haproxy.cfg
 cd ..
-
+#
 #v2ray
 bash <(curl -L -s https://install.direct/go.sh)
 wget /etc/v2ray/config.json https://raw.githubusercontent.com/RoFatNya/config/master/v2ray/config.json
-
+#
 #bbr
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
-
+#
 #shadowsocks
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
-
+#
 #speedtest-cli
 wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 chmod +x speedtest-cli
-
+#
 #port
 firewall-cmd --zone=public --add-port=777/tcp --permanent
 firewall-cmd --zone=public --add-port=777/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=7777/tcp --permanent
 firewall-cmd --zone=public --add-port=7777/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --zone=public --add-port=443/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=840/tcp --permanent
 firewall-cmd --zone=public --add-port=840/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=850/tcp --permanent
 firewall-cmd --zone=public --add-port=850/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=888/tcp --permanent
 firewall-cmd --zone=public --add-port=888/udp --permanent
-
+#
 firewall-cmd --zone=public --add-port=10000-11000/tcp --permanent
 firewall-cmd --zone=public --add-port=10000-11000/udp --permanent
-
+#
 firewall-cmd --reload
-
+#
 reboot
